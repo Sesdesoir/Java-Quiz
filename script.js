7cbfc005aa32a39042856ac0bc90d54a45ff1c67
Leslie Williams <Seslie2009@yahoo.com>
2021-01-17 14:53:50 -0800

    Decided to center buttons. First question displays on start press properly. Now we need further functionality.

--- script.js
+++ script.js
@@ -3,78 +3,78 @@ var question = document.querySelector("#question");
 var multiChoiceDiv = document.querySelector("#multiChoice");
 var startButton = document.querySelector("#start");
 var header = document.querySelector("#headDiv");
-var a = document.querySelector("a");
-var b = document.querySelector("b");
-var c = document.querySelector("c");
-var d = document.querySelector("d");
+var a = document.querySelector("#a");
+var b = document.querySelector("#b");
+var c = document.querySelector("#c");
+var d = document.querySelector("#d");
 //Variables
 var timer = 6;
 //Array of predefined question objects *currently no constructor for more objects*
 var questions = [{
-"Question#": "How do you display something to the console?",
+questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
 ChoiceD:"console.log();"
 },
 {
-"Question#": "How do you display something to the console?",
+questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
 ChoiceD:"console.log();"
 },
 {
-"Question#": "How do you display something to the console?",
+questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
 ChoiceD:"console.log();"
 },
 {
-"Question#": "How do you display something to the console?",
+questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
 ChoiceD:"console.log();"
 },
 {
-"Question#": "How do you display something to the console?",
+questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
 ChoiceD:"console.log();"
 },
 {
-"Question#": "How do you display something to the console?",
+questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
 ChoiceD:"console.log();"
 },
 {
-"Question#": "How do you display something to the console?",
+questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
 ChoiceD:"console.log();"
 },
 {
-"Question#": "How do you display something to the console?",
+questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
 ChoiceD:"console.log();"
 },
 {
-"Question#": "How do you display something to the console?",
+questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
 ChoiceD:"console.log();"
 },
 {
-"Question#": "How do you display something to the console?",
+questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
@@ -99,7 +99,22 @@ startButton.addEventListener("click", function(event){
             clearInterval(interval);
         }
     }, 1000);
+    question.style.visibility = "visible";
+    a.style.visibility = "visible";
+    b.style.visibility = "visible";
+    c.style.visibility = "visible";
+    d.style.visibility = "visible";
+    pickQuestion();
 });
+
+function pickQuestion() {
+var randomQuestion = questions[Math.floor(Math.random()*questions.length)];
+question.innerText = randomQuestion.questionNum;
+a.innerText += (" " + randomQuestion.ChoiceA);
+b.innerText += (" " + randomQuestion.ChoiceB);
+c.innerText += (" " + randomQuestion.ChoiceC);
+d.innerText += (" " + randomQuestion.ChoiceD);
+};
 
 //Multiple choice div where the actual questions take place
 //question.innerText = questions[Math.floor(Math.random()*questions.length)];
