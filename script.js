4bb428677697f9fce17e03fe5b8a9626ef3537f8
Leslie Williams <Seslie2009@yahoo.com>
2021-01-17 13:44:53 -0800

    added array of question objects, timer starts and stops, question buttons added with random selector set up.

--- script.js
+++ script.js
@@ -1,3 +1,114 @@
+//html elements
 var question = document.querySelector("#question");
 var multiChoiceDiv = document.querySelector("#multiChoice");
 var startButton = document.querySelector("#start");
+var header = document.querySelector("#headDiv");
+//Variables
+var timer = 6;
+//Array of predefined question objects *currently no constructor for more objects*
+var questions = [{
+"Question#": "How do you display something to the console?",
+ChoiceA:"print();",
+ChoiceB:"display();",
+ChoiceC:"console();",
+ChoiceD:"console.log();"
+},
+{
+"Question#": "How do you display something to the console?",
+ChoiceA:"print();",
+ChoiceB:"display();",
+ChoiceC:"console();",
+ChoiceD:"console.log();"
+},
+{
+"Question#": "How do you display something to the console?",
+ChoiceA:"print();",
+ChoiceB:"display();",
+ChoiceC:"console();",
+ChoiceD:"console.log();"
+},
+{
+"Question#": "How do you display something to the console?",
+ChoiceA:"print();",
+ChoiceB:"display();",
+ChoiceC:"console();",
+ChoiceD:"console.log();"
+},
+{
+"Question#": "How do you display something to the console?",
+ChoiceA:"print();",
+ChoiceB:"display();",
+ChoiceC:"console();",
+ChoiceD:"console.log();"
+},
+{
+"Question#": "How do you display something to the console?",
+ChoiceA:"print();",
+ChoiceB:"display();",
+ChoiceC:"console();",
+ChoiceD:"console.log();"
+},
+{
+"Question#": "How do you display something to the console?",
+ChoiceA:"print();",
+ChoiceB:"display();",
+ChoiceC:"console();",
+ChoiceD:"console.log();"
+},
+{
+"Question#": "How do you display something to the console?",
+ChoiceA:"print();",
+ChoiceB:"display();",
+ChoiceC:"console();",
+ChoiceD:"console.log();"
+},
+{
+"Question#": "How do you display something to the console?",
+ChoiceA:"print();",
+ChoiceB:"display();",
+ChoiceC:"console();",
+ChoiceD:"console.log();"
+},
+{
+"Question#": "How do you display something to the console?",
+ChoiceA:"print();",
+ChoiceB:"display();",
+ChoiceC:"console();",
+ChoiceD:"console.log();"
+}];
+
+//Quiz Start
+startButton.addEventListener("click", function(event){
+    event.preventDefault;
+    startButton.style.display = "none";
+    var clock = document.createElement("p");
+    clock.innerText = timer;
+    header.appendChild(clock);
+    clock.style.textAlign = "center";
+    clock.style.fontSize = "40px";
+    clock.style.fontWeight= "bold";
+    clock.style.color= "red";
+    var interval = setInterval(function(){
+        timer--
+        clock.innerText = timer;
+        if (timer === 0){
+            clearInterval(interval);
+        }
+    }, 1000);
+});
+
+//Multiple choice div where the actual questions take place
+question.innerText = questions[Math.floor(Math.random()*questions.length)];
+var a = document.createElement("button");
+var b = document.createElement("button");
+var c = document.createElement("button");
+var d = document.createElement("button");
+multiChoiceDiv.appendChild(a);
+multiChoiceDiv.appendChild(b);
+multiChoiceDiv.appendChild(c);
+multiChoiceDiv.appendChild(d);
+
+
+   
+
+
