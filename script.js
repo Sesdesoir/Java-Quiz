e75b601c2596ab2f58f04581ac46d3deae379bc5
Leslie Williams <Seslie2009@yahoo.com>
2021-01-17 19:39:26 -0800

    took splice out of pickQuestion function. Answers now correspond to their proper questions. We are essentially fully functional. Could use some flare. Need at least 5 more questions and need to add local storage feature for high scores.

--- script.js
+++ script.js
@@ -11,96 +11,108 @@ var letterheadA = document.querySelector("#choiceA");
 var letterheadB = document.querySelector("#choiceB");
 var letterheadC = document.querySelector("#choiceC");
 var letterheadD = document.querySelector("#choiceD");
-//Created Element & Styling
+//Created Element & Styling (could technically preset it in html with css display none....oh well)
 var clock = document.createElement("p");
 clock.style.textAlign = "center";
 clock.style.fontSize = "40px";
 clock.style.fontWeight= "bold";
 clock.style.color= "red";
 //Variables
-var timer = 6;
+var timer = 60;
 var score = 0;
 var index;
+var isOver = false;
 //Array of predefined question objects *currently no constructor for more objects*
-var questions = [{
-questionNum: "How do you display something to the console?",
-ChoiceA:"print();",
-ChoiceB:"display();",
-ChoiceC:"console();",
-ChoiceD:"console.log();",
-answer: "console.log();"
-},
+var questions = [
+//Q1
 {
 questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
 ChoiceD:"console.log();",
-answer: "console.log();"
+answer:"console.log();"
 },
+//Q2
+{
+questionNum: "In a setInterval(function(){} , time) what is the value of time?",
+ChoiceA:"Seconds",
+ChoiceB:"lilseconds",
+ChoiceC:"miliseconds",
+ChoiceD:"minutes",
+answer:"miliseconds"
+},
+//Q3
+{
+questionNum: "What kind of loop do we use to iterate over an array?",
+ChoiceA:"while loop",
+ChoiceB:"for loop",
+ChoiceC:"if loop",
+ChoiceD:"else loop",
+answer:"for loop"
+},
+//Q4
+{
+questionNum: "function myFunction(name){}; What is 'name' called?",
+ChoiceA:"An argument",
+ChoiceB:"A query",
+ChoiceC:"A variable",
+ChoiceD:"A function",
+answer:"An argument"
+},
+//Q5
+{
+questionNum: "How do you declare a variable in javascript?",
+ChoiceA:"int i = 0;",
+ChoiceB:"char letter = 'y';",
+ChoiceC:"var fancy === 'feast';",
+ChoiceD:"var variable = true;",
+answer:"var variable = true;"
+},
+//Q6
 {
 questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
 ChoiceD:"console.log();",
-answer: "console.log();"
+answer:"console.log();"
 },
+//Q7
 {
 questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
 ChoiceD:"console.log();",
-answer: "console.log();"
+answer:"console.log();"
 },
+//Q8
 {
 questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
 ChoiceD:"console.log();",
-answer: "console.log();"
+answer:"console.log();"
 },
+//Q9
 {
 questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
 ChoiceD:"console.log();",
-answer: "console.log();"
+answer:"console.log();"
 },
+//Q10
 {
 questionNum: "How do you display something to the console?",
 ChoiceA:"print();",
 ChoiceB:"display();",
 ChoiceC:"console();",
 ChoiceD:"console.log();",
-answer: "console.log();"
-},
-{
-questionNum: "How do you display something to the console?",
-ChoiceA:"print();",
-ChoiceB:"display();",
-ChoiceC:"console();",
-ChoiceD:"console.log();",
-answer: "console.log();"
-},
-{
-questionNum: "How do you display something to the console?",
-ChoiceA:"print();",
-ChoiceB:"display();",
-ChoiceC:"console();",
-ChoiceD:"console.log();",
-answer: "console.log();"
-},
-{
-questionNum: "How do you display something to the console?",
-ChoiceA:"print();",
-ChoiceB:"display();",
-ChoiceC:"console();",
-ChoiceD:"console.log();",
-answer: "console.log();"
+answer:"console.log();"
 }];
 //Essentially being used to console track how many questions are left. This is the beginning and should be 10
 console.log("Beginning " + questions.length);
@@ -113,15 +125,27 @@ function pickQuestion() {
     b.innerText = randomQuestion.ChoiceB;
     c.innerText = randomQuestion.ChoiceC;
     d.innerText = randomQuestion.ChoiceD;
-    questions.splice(index,1);
     };
 function intervalSet(){
     var interval = setInterval(function(){
     timer--
     clock.innerText = timer;
     if (timer === 0){
-        question.innerText = "Quizz over you scored " + score + " points!"; 
+        question.innerText = "Time over you scored " + score + " points!"; 
         console.log(score + "points Quiz Over");
+        a.style.visibility = "hidden";
+        b.style.visibility = "hidden";
+        c.style.visibility = "hidden";
+        d.style.visibility = "hidden";
+        letterheadA.style.visibility = "hidden";
+        letterheadB.style.visibility = "hidden";
+        letterheadC.style.visibility = "hidden";
+        letterheadD.style.visibility = "hidden";
+        clearInterval(interval);
+    }
+    if(isOver === true){
+        question.innerText = "Quizz over you scored " + score + " points!";
+        clock.style.display = "none";
         a.style.visibility = "hidden";
         b.style.visibility = "hidden";
         c.style.visibility = "hidden";
@@ -152,159 +176,125 @@ startButton.addEventListener("click", function(event){
     pickQuestion();
     console.log(questions.length + " questions left.");
 });
+
 //Button click for Multiple Choice A
 a.addEventListener("click", function(){
     if (questions.length> 1){
-        if(questions[index].answer === a.innerText ){
+        if(questions[index].answer === a.innerText){
             score += 10;
-            pickQuestion();
             console.log(score + " points");
             console.log(questions.length + " questions left. ");
-            console.log(index + " random index");
+            questions.splice(index,1);
+            pickQuestion();
             }
         else{
             score -= 10;
-            pickQuestion();
             console.log(score + "points");
             console.log(questions.length + " questions left.");
+            questions.splice(index,1);
+            pickQuestion();
         }
     }
     else{
-        if(questions[index].answer === a.innerText ){
+        if(questions[index].answer === a.innerText){
             score += 10;
             score += (timer * 10);
             console.log(score + "points Quiz Over");
-            a.style.visibility = "hidden";
-            b.style.visibility = "hidden";
-            c.style.visibility = "hidden";
-            d.style.visibility = "hidden";
-            letterheadA.style.visibility = "hidden";
-            letterheadB.style.visibility = "hidden";
-            letterheadC.style.visibility = "hidden";
-            letterheadD.style.visibility = "hidden";
+            isOver = true;
         }
         else{
             score -= 10;
             score += (timer * 10);
             console.log(score + "points Quiz Over");
-            a.style.visibility = "hidden";
-            b.style.visibility = "hidden";
-            c.style.visibility = "hidden";
-            d.style.visibility = "hidden";
-            letterheadA.style.visibility = "hidden";
-            letterheadB.style.visibility = "hidden";
-            letterheadC.style.visibility = "hidden";
-            letterheadD.style.visibility = "hidden";
+            isOver = true;
         }            
     }
 });
+
 //Button click for Multiple Choice B
 b.addEventListener("click", function(){
     if (questions.length> 1){
-        if(questions[index].answer === b.innerText ){
+        if(questions[index].answer === b.innerText){
             score += 10;
-            pickQuestion();
             console.log(score + " points");
             console.log(questions.length + " questions left. ");
-            console.log(index + " random index");
+            questions.splice(index,1);
+            pickQuestion();
             }
         else{
             score -= 10;
-            pickQuestion();
             console.log(score + "points");
             console.log(questions.length + " questions left.");
+            questions.splice(index,1);
+            pickQuestion();
         }
     }
     else{
-        if(questions[index].answer === b.innerText ){
+        if(questions[index].answer === b.innerText){
             score += 10;
             score += (timer * 10);
             console.log(score + "points Quiz Over");
-            a.style.visibility = "hidden";
-            b.style.visibility = "hidden";
-            c.style.visibility = "hidden";
-            d.style.visibility = "hidden";
-            letterheadA.style.visibility = "hidden";
-            letterheadB.style.visibility = "hidden";
-            letterheadC.style.visibility = "hidden";
-            letterheadD.style.visibility = "hidden";
+            isOver = true;
         }
         else{
             score -= 10;
             score += (timer * 10);
             console.log(score + "points Quiz Over");
-            a.style.visibility = "hidden";
-            b.style.visibility = "hidden";
-            c.style.visibility = "hidden";
-            d.style.visibility = "hidden";
-            letterheadA.style.visibility = "hidden";
-            letterheadB.style.visibility = "hidden";
-            letterheadC.style.visibility = "hidden";
-            letterheadD.style.visibility = "hidden";
+            isOver = true;
         }            
     }
 });
+
 //Button click for Multiple Choice C
 c.addEventListener("click", function(){
     if (questions.length> 1){
-        if(questions[index].answer === c.innerText ){
+        if(questions[index].answer === c.innerText){
             score += 10;
-            pickQuestion();
             console.log(score + " points");
             console.log(questions.length + " questions left. ");
-            console.log(index + " random index");
+            questions.splice(index,1);
+            pickQuestion();
             }
         else{
             score -= 10;
-            pickQuestion();
             console.log(score + "points");
             console.log(questions.length + " questions left.");
+            questions.splice(index,1);
+            pickQuestion();
         }
     }
     else{
-        if(questions[index].answer === c.innerText ){
+        if(questions[index].answer === c.innerText){
             score += 10;
             score += (timer * 10);
             console.log(score + "points Quiz Over");
-            a.style.visibility = "hidden";
-            b.style.visibility = "hidden";
-            c.style.visibility = "hidden";
-            d.style.visibility = "hidden";
-            letterheadA.style.visibility = "hidden";
-            letterheadB.style.visibility = "hidden";
-            letterheadC.style.visibility = "hidden";
-            letterheadD.style.visibility = "hidden";
+            isOver = true;
         }
         else{
             score -= 10;
             score += (timer * 10);
             console.log(score + "points Quiz Over");
-            a.style.visibility = "hidden";
-            b.style.visibility = "hidden";
-            c.style.visibility = "hidden";
-            d.style.visibility = "hidden";
-            letterheadA.style.visibility = "hidden";
-            letterheadB.style.visibility = "hidden";
-            letterheadC.style.visibility = "hidden";
-            letterheadD.style.visibility = "hidden";
+            isOver = true;
         }            
     }
 });
+
 //Button click for Multiple Choice D
 d.addEventListener("click", function(){
     if (questions.length> 1){
-        if(questions[index].answer === d.innerText ){
+        if(questions[index].answer === d.innerText){
             score += 10;
-            pickQuestion();
             console.log(score + " points");
             console.log(questions.length + " questions left. ");
-            console.log(index + " random index");
+            questions.splice(index,1);
+            pickQuestion();
             }
         else{
             score -= 10;
-            pickQuestion();
             console.log(score + "points");
             console.log(questions.length + " questions left.");
+            questions.splice(index,1);
+            pickQuestion();
         }
     }
     else{
@@ -312,27 +302,13 @@ d.addEventListener("click", function(){
             score += 10;
             score += (timer * 10);
             console.log(score + "points Quiz Over");
-            a.style.visibility = "hidden";
-            b.style.visibility = "hidden";
-            c.style.visibility = "hidden";
-            d.style.visibility = "hidden";
-            letterheadA.style.visibility = "hidden";
-            letterheadB.style.visibility = "hidden";
-            letterheadC.style.visibility = "hidden";
-            letterheadD.style.visibility = "hidden";
+            isOver = true;
         }
         else{
             score -= 10;
             score += (timer * 10);
             console.log(score + "points Quiz Over");
-            a.style.visibility = "hidden";
-            b.style.visibility = "hidden";
-            c.style.visibility = "hidden";
-            d.style.visibility = "hidden";
-            letterheadA.style.visibility = "hidden";
-            letterheadB.style.visibility = "hidden";
-            letterheadC.style.visibility = "hidden";
-            letterheadD.style.visibility = "hidden";
+            isOver = true;
         }            
     }
 });
